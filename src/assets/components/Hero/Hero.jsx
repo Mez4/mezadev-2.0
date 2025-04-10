afe56166ae11675ccbdc6dfacee926f4b8779cd3
Mez4 <mezajesus318@gmail.com>
2025-04-09 22:04:28 -0600

    cambio de fuente

--- src/assets/components/Hero/Hero.jsx
+++ src/assets/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ function Hero() {
         pointerColor={pointerColor}
         size={size}
       >
-        <div className="text-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold z-10 font-sans dark:text-neutral-900">
+        <div className="text-6xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold z-10 font-sans dark:text-neutral-900">
           Alejandro Meza <br />
           <FlipWords
             words={words}
